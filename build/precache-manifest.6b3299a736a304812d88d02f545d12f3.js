e4885bb7b1567a5665787ce8e4bf214b28947dd8
abkr08 <abubakaribrahim08@yahoo.com>
2019-10-15 23:13:01 +0300

    Added the built files

---
+++ build/precache-manifest.6b3299a736a304812d88d02f545d12f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8744f18ba037b09d1931",
+    "url": "/static/css/main.8cda776c.chunk.css"
+  },
+  {
+    "revision": "8744f18ba037b09d1931",
+    "url": "/static/js/main.8744f18b.chunk.js"
+  },
+  {
+    "revision": "ac82d464206c3d457548",
+    "url": "/static/js/1.ac82d464.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8335e87d433bae0469e9401605444320",
+    "url": "/static/media/p37605.8335e87d.png"
+  },
+  {
+    "revision": "d21333e9f74edb8bf7d3013f129d03f9",
+    "url": "/static/media/iphone_trap_remix.d21333e9.mp3"
+  },
+  {
+    "revision": "62fbd90152e7ed515440a264c024f3f2",
+    "url": "/index.html"
+  }
+];
