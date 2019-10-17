c0a9e8ec37d7fb8e21dbfd16961847d84893da48
abkr08 <abubakaribrahim08@yahoo.com>
2019-10-18 00:04:25 +0300

    Added the latest build

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ const io = require('socket.io').listen(server);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, 'build')));
 
-  app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
+  // app.get('/*', (req, res) => {
+  //   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  // });
 }
 
 //Importing files here
