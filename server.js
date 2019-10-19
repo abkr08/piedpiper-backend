8cc62846d0facf9e02e80143c9ba5d6b3ece0516
abkr08 <abubakaribrahim08@yahoo.com>
2019-10-20 00:57:48 +0300

    Added the latest build

--- server.js
+++ server.js
@@ -7,15 +7,12 @@ const app = express();
 const server = app.listen(8082); 
 const io = require('socket.io').listen(server); 
 
+app.use(express.static(path.join(__dirname, './build')));
 
-
-
-// var httpServer = http.createServer(app);
-// var httpsServer = https.createServer(options, app);
 if (process.env.NODE_ENV === "production") {
   console.log('production');
-  app.use(express.static(__dirname + '/client/build'));
-  // app.use(express.static(__dirname + '/client/build'));
+  app.use(express.static(path.join(__dirname, './build')));
+  // app.use(express.static((__dirname + '/client/build'));
   // app.get('/*', (req, res) => {
   //   res.sendFile(path.join(__dirname, 'build', 'index.html'));
   // });
@@ -210,5 +207,5 @@ function sendTo(socket, message) {
    socket.send(JSON.stringify(message)); 
 }
 
-const PORT = process.env.PORT; //|| 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on ${PORT}..`));
